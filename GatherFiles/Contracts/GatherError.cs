599b8b95dd276aa7505d5564ca04ab14336cead1
Markus Iorio <mbalzert@gmail.com>
2024-11-18 20:06:08 +0100

    from exc

--- GatherFiles/Contracts/GatherError.cs
+++ GatherFiles/Contracts/GatherError.cs
@@ -2,6 +2,7 @@ using SharedKernel.Abstractions;
 
 namespace GatherFiles.Contracts;
 
-public readonly record struct GatherError(string Message) : IFrom<string, GatherError> {
+public readonly record struct GatherError(string Message) : IFrom<string, GatherError>, IFrom<Exception, GatherError> {
     public static GatherError From(string value) => new(value);
+    public static GatherError From(Exception value) => new(value.Message);
 }
