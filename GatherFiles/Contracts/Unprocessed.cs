7029c9340d8158ffc151f75a7329c84521e21439
Markus Iorio <mbalzert@gmail.com>
2024-11-18 20:06:17 +0100

    from ienumerable

--- GatherFiles/Contracts/Unprocessed.cs
+++ GatherFiles/Contracts/Unprocessed.cs
@@ -2,7 +2,7 @@ using SharedKernel.Abstractions;
 
 namespace GatherFiles.Contracts;
 
-public readonly record struct Unprocessed(ICollection<FileContent> Files) : IFrom<ICollection<FileContent>, Unprocessed> {
-    public static Unprocessed From(ICollection<FileContent> value) => new(value);
+public readonly record struct Unprocessed(ICollection<FileContent> Files) : IFrom<IEnumerable<FileContent>, Unprocessed> {
+    public static Unprocessed From(IEnumerable<FileContent> value) => new(value.ToArray());
 }
 
